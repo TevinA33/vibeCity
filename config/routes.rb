5bc3407a4d26de092659b18262da1b85d94fe763
TevinA33 <tevina@email.uscupstate.edu>
2017-02-25 05:38:33 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ ources :usersails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+  #  root 'application#hello' 
+     root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
