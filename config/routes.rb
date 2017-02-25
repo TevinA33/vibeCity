5476146f8f7563d76d34d0b7ebca8c9eeaad598a
TevinA33 <tevina@email.uscupstate.edu>
2017-02-25 04:56:50 +0000

    all 3 roots open

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
     root 'application#hello' 
-  # root 'welcome#hello'
-  # root 'users#index'
+    root 'welcome#hello'
+    root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
